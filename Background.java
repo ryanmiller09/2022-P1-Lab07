6223366718176f59d43e4329233b2d796b695873
13195 <ryan.miller@vtsd.com>
2022-11-30 08:41:14 -0500

    Lab 07

---
+++ Background.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.applet.*;
+
+public class Background {
+    public static void drawBackground(Graphics g) {
+
+        g.setColor(Color.blue);
+        g.fillRect(0,0,1000,500);
+        g.setColor(Color.black);
+        g.fillRect(0,500,1000,500);
+
+
+        }
+    }
+
---
+++ DrawPicture.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import java.applet.*;
+
+public class DrawPicture extends Applet{
+
+
+
+}
